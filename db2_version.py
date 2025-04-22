3dc98c94f0759acd24b0a73fd3a5a945ac21eb52
pooneth <jtharel@yahoo.com>
2025-04-22 09:42:56 -0600

    Update db2_version.py

--- db2_version.py
+++ db2_version.py
@@ -5,7 +5,7 @@ import jpype
 
 jdbc_driver = "com.ibm.db2.jcc.DB2Driver"
 jdbc_url = "jdbc:db2://10.1.1.251:50000/{DB_Name}:sslConnection=true;"
-jdbc_driver_path = "/root/db2/ncdb2/jcc-11.5.9.0.jar"
+jdbc_driver_path = "./jcc-11.5.9.0.jar"
 username = "jimmy"
 password = "password"
 driver_class = "com.ibm.db2.jcc.DB2Driver"
