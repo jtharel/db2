bde726b733e4750bb8655299febf0d0bb489611a
pooneth <jtharel@yahoo.com>
2025-04-22 09:45:59 -0600

    Create db2_currentuser.py

---
+++ db2_currentuser.py
@@ -0,0 +1,38 @@
+#Display current user via a DB query
+
+import jaydebeapi
+import jpype
+
+jdbc_driver_path = "./jcc-11.5.9.0.jar"  
+
+host = "10.1.1.251"  
+port = "50000" 
+db_name = "{db_name}"
+schema_name = "{scheuma_name}"
+url = f"jdbc:db2://{host}:{port}/{db_name}:sslConnection=true;loginTimeout=30;socketTimeout=60;"
+
+username = "jimmy"
+password = "password"
+
+jdbc_driver = "com.ibm.db2.jcc.DB2Driver"
+jdbc_url = url
+driver_class = "com.ibm.db2.jcc.DB2Driver"
+
+jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.class.path=" + jdbc_driver_path)
+
+conn = jaydebeapi.connect(jdbc_driver, jdbc_url, [username, password], jdbc_driver_path)
+
+cursor = conn.cursor()
+
+cursor.execute(f"SELECT USER FROM SYSIBM.SYSDUMMY1")
+
+tables = cursor.fetchall()
+
+for table in tables:
+    print(f"Current User:", table[0])
+
+cursor.close()
+conn.close()
+
+jpype.shutdownJVM()
+
