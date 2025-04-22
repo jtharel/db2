9e73c42340b8d085b8afcf93f0a13d6f8a4e1bfe
pooneth <jtharel@yahoo.com>
2025-04-22 09:37:50 -0600

    Create db2_version.py

---
+++ db2_version.py
@@ -0,0 +1,23 @@
+#Shows DB2 version
+
+import jaydebeapi
+import jpype
+
+jdbc_driver = "com.ibm.db2.jcc.DB2Driver"
+jdbc_url = "jdbc:db2://10.1.1.251:50000/{DB_Name}:sslConnection=true;"
+jdbc_driver_path = "/root/db2/ncdb2/jcc-11.5.9.0.jar"
+username = "jimmy"
+password = "password"
+driver_class = "com.ibm.db2.jcc.DB2Driver"
+
+if not jpype.isJVMStarted():
+    jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.class.path=" + jdbc_driver_path)
+
+conn = jaydebeapi.connect(jdbc_driver, jdbc_url, [username, password], jdbc_driver_path)
+
+conn_jdbc = conn.jconn
+meta = conn_jdbc.getMetaData()
+print("DB2 Product Name:", meta.getDatabaseProductName())
+print("DB2 Version:", meta.getDatabaseProductVersion())
+
+conn.close()
